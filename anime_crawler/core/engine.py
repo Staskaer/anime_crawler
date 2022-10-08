b2394abeeadbd589613fc373e2b15eb12fc291fc
ppxxxg22 <liujiaxin011121@gmail.com>
2022-10-08 17:41:23 +0800

    更新engine与downloader的逻辑

--- anime_crawler/core/engine.py
+++ anime_crawler/core/engine.py
@@ -1,10 +1,11 @@
 from anime_crawler.core.download import Downloader
 from anime_crawler.core.imageio import ImageIO
+from anime_crawler.core.requests_generator import RequestsGenerator
 from anime_crawler.core.requests_repository import RequestsRepository
-from anime_crawler.utils.image_item import ImageItem
-from anime_crawler.settings import *
+from anime_crawler.utils.bloomfilter import BloomFilter
 from anime_crawler.utils.options import Options
 from anime_crawler.utils.stop_conditions import StopConditions
+from anime_crawler.settings import *
 
 '''
 Engine类用于调度，主要有以下几个作用:
@@ -20,16 +21,25 @@ Engine类用于调度，主要有以下几个作用:
 
 class Engine:
     def __init__(self,
-                 requests_repository: RequestsRepository,
-                 downloader: Downloader,
-                 requests_generator) -> None:
-        self._downloader = downloader()
-        self._imageio = ImageIO()
+                 requests_generator_: RequestsGenerator,
+                 stop_conditions: StopConditions,
+                 requests_repository_: RequestsRepository,
+                 filter_: BloomFilter,
+                 downloader_: Downloader,
+                 imageio_: ImageIO,
+                 options_: Options) -> None:
+        self._downloader = downloader_(requests_generator=requests_generator_,
+                                       requests_repository=requests_repository_,
+                                       imageio=imageio_,
+                                       filter=filter_,
+                                       options=options_)
+        self._image_io = imageio_()
+        self._stop_conditions = stop_conditions()
 
-    def scheduler(self, stop: StopConditions) -> None:
+    @classmethod
+    def run():
+        ...
+
+    def scheduler(self) -> None:
         # TODO 停止条件需要加入
-        while 1:
-            self._downloader.fill_download_queue()
-            if ((item := self._downloader.pop()) is None):
-                continue
-            self._imageio.add(item)
+        ...
--- anime_crawler/utils/stop_conditions.py
+++ anime_crawler/utils/stop_conditions.py
@@ -1,3 +1,31 @@
+from anime_crawler.utils.fileio import FileIO
+
 
 class StopConditions:
-    ...
+    def __init__(self, fileio_: FileIO = FileIO) -> None:
+        self._lasttingtime = None  # 持续时间(min):int
+        self._size_of_pics = None  # 占用空间(MB):int
+        self._nums = None  # 下载的数目
+        self._timerange = None  # 给定运行时间
+
+        self._circle = True  # 是否重复执行
+        self._fileio = fileio_()  # FileIO接口
+
+        self._start_time = 0  # 用于判断的变量
+        self._last_nums = 0
+        self._last_size = 0
+
+    def valid(self) -> bool:
+        '''
+        验证是否到达停止条件
+
+        Returns:
+            bool: 停止返回1，否则返回0
+        '''
+        if self._circle is False:
+            ...
+        else:
+            ...
+
+    def add_stop_conditions(self, type_=""):
+        ...
