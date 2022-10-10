9ea6ebc0faa41ba596f4ce208d7c85d920daa256
ppxxxg22 <liujiaxin011121@gmail.com>
2022-10-10 17:35:53 +0800

    好！

---
+++ demo.py
@@ -0,0 +1,13 @@
+# 导入engine和stopconditions！
+from anime_crawler import StopConditions, Engine
+# 导入自带的RequestsGenerator！
+from anime_crawler.core.requests_generator import RequestsGenerator
+
+# 实例化一个停止条件！
+a = StopConditions()
+# 添加停止条件，这里是爪巴取10分钟后停止呢！
+a.add_stop_condition(lastingtime=10)
+# 开始爪巴！
+Engine.run(RequestsGenerator, a)
+
+# 可以查看一下anime_crawler/settings.py的配置文件，可以自己修改一下哦！
