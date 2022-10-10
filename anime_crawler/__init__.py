67769d92048659a1bfe7465f774daa9c5deff74e
ppxxxg22 <liujiaxin011121@gmail.com>
2022-10-10 22:07:53 +0800

    更新！

--- anime_crawler/__init__.py
+++ anime_crawler/__init__.py
@@ -1,4 +1,14 @@
+# 很有用的东西
 from anime_crawler.core.engine import Engine
+from anime_crawler.http.requests_block import RequestsBlock
 from anime_crawler.utils.stop_conditions import StopConditions
 from anime_crawler.core.requests_generator import RequestsGenerator
 from anime_crawler.settings import *
+
+
+# 没那么有用的东西
+from anime_crawler.utils.image_item import ImageItem
+from anime_crawler.utils.logger import Logger
+from anime_crawler.utils.fileio import FileIO
+from anime_crawler.core.requests_repository import RequestsRepository
+from anime_crawler.core.download import Downloader
